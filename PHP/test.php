3c221f556b8bf6c45f83f15daaf223e97e0acfd0
AchrafJALAL <37595402+ABUNAI27@users.noreply.github.com>
2022-06-17 09:17:11 +0200

    Update test.php

--- PHP/test.php
+++ PHP/test.php
@@ -16,6 +16,12 @@ curl_close($ch);
 echo "Raw Data:<br />";
 echo("$data");
 
+$data_tab = str_split($data,33);
+echo "Tabular Data:<br />";
+for($i=0, $size=count($data_tab); $i<$size; $i++){
+echo "Trame $i: $data_tab[$i]<br />";
+}
+
 $trame = $data_tab[1];
 // décodage avec des substring
 $t = substr($trame,0,1);
