004a23f1c41e1ba6740a08ce714c8e26995b1adf
AchrafJALAL <37595402+ABUNAI27@users.noreply.github.com>
2022-06-17 09:01:10 +0200

    Add files via upload

---
+++ PHP/TESTPHP.php
@@ -0,0 +1,81 @@
+<?php
+
+    session_start();
+?>
+
+<?php
+$ch = curl_init();
+curl_setopt(
+$ch,
+CURLOPT_URL,
+"http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=9999");
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$data = curl_exec($ch);
+curl_close($ch);
+echo "Raw Data:<br />";
+echo("$data");
+
+$trame = $data_tab[1];
+// décodage avec des substring
+$t = substr($trame,0,1);
+$o = substr($trame,1,4);
+
+// décodage avec sscanf
+list($t, $o, $r, $c, $n, $v, $a, $x, $year, $month, $day, $hour, $min, $sec) =
+sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
+echo("<br />$t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec<br />");
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> GreenRoad</title>
+    <link rel="stylesheet" href="../CSS/contact.css?v=<?php echo time(); ?>"> 
+</head>
+
+
+<body>
+    <section class="login">
+        <?php
+        
+            if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+                echo "<a class=\"btn1\" href=\"../PHP/register.php\">Inscription</a>";
+                echo "<a class=\"btn2\" href=\"../PHP/login.php\">Connexion</a>";
+            }else{
+                echo "<a href=\"../PHP/userpage.php\"><img src=\"../images/profillogo.png\"></a>";
+                echo "<a class=\"btn3\" href=\"../PHP/logout.php\" >Déconnexion</a>";
+
+            }
+        ?>
+
+    </section>
+    <nav>
+        <a href="../php/MainPage.php">
+            <img src="../IMAGES/GreenRoad.gif">
+        </a>
+        <h1>GreenRoad</h1>
+        <div class="onglets">
+            <ul>
+                </li><a href="../php/MainPage.php">Accueil</a></li>
+                </li><a href="../php/caaapa.php">L'équipe</a></li>
+                </li><u><a href="../php/contact.php">Contact</a></u></li>
+                </li><a href="../php/faq.php">FAQ</a></li>
+                </li><a href="../php/cartographie.php">Cartographie</a></li>
+                </li><a href="../php/statsetdonnees.php">Statistiques</a></li>
+                </li><a href="../php/pierrito.php" class="pierrito">Pierrito Game<span><img src="../IMAGES/pierrito.png"/></span></a></li>
+            </ul>
+        </div>
+
+    </nav>
+
+</body>
+
+<!--Footer-->
+<?php include_once('footer.php'); ?>
+
+</html>
