118f58afc31a910ca906e05f8e768a03acca8ecd
AchrafJALAL <37595402+ABUNAI27@users.noreply.github.com>
2022-06-17 09:36:08 +0200

    Delete PHP directory

--- PHP/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db= mysqli_connect("594bdbaa505f", "nwOvG8o3Ct_greenroad_", "oHGtSU0jsdukvaAW", "9WygKP7524_greenroad_");
-if(!$db) {
-    echo "Connexion non établie.";
-    exit;
-}
-
-?>
