73647fce1526493196f484a066b1a0ebebc0c94b
AchrafJALAL <37595402+ABUNAI27@users.noreply.github.com>
2022-06-21 12:39:32 +0200

    Update and rename Actionneur to Actionneur.php

--- GreenRoad/PHP/Actionneur.php
+++ GreenRoad/PHP/Actionneur.php
@@ -48,7 +48,38 @@
         </div>
 
     </nav>
+<form action="" method="post">
 
+<input type="submit" class="boutonLED" name="submit" value="Allumer la led rouge">
+<input type="submit" class="boutonLED" name="submit2" value="Allumer la led bleue">
+<input type="submit" class="boutonLED" name="submit3" value="Eteindre la led">
+</form>
+<?php
+
+if(isset($_POST['submit'])) {
+
+        //$monUrl="http://projets-tomcat.isep.fr:8080/appService/?ACTION=COMMAND&TEAM=G3B1&TRAME=1G3B141425411425687423";   definir trame
+
+        header("Location: $monUrl");
+        exit;
+}
+
+if(isset($_POST['submit2'])) {
+
+    //$monUrl="http://projets-tomcat.isep.fr:8080/appService/?ACTION=COMMAND&TEAM=G3B1&TRAME=1G3B131425411425687423";   definir trame
+
+    header("Location: $monUrl");
+    exit;
+}
+
+if(isset($_POST['submit3'])) {
+
+    //$monUrl="http://projets-tomcat.isep.fr:8080/appService/?ACTION=COMMAND&TEAM=G3B1&TRAME=1G3B151425411425687423";   definir trame
+
+    header("Location: $monUrl");
+    exit;
+}
+?>
 </body>
 
 <!--Footer-->
