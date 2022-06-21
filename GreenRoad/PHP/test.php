389d1374196b95ff272e6822d6472b2acb9047c3
AchrafJALAL <37595402+ABUNAI27@users.noreply.github.com>
2022-06-21 12:15:51 +0200

    Update test.php

--- GreenRoad/PHP/test.php
+++ GreenRoad/PHP/test.php
@@ -58,13 +58,19 @@ curl_setopt($ch, CURLOPT_HEADER, FALSE);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $data = curl_exec($ch);
 curl_close($ch);
-echo "Raw Data:<br />";
-echo("$data");
 
-$data_tab = str_split($data,33);
-echo "Tabular Data:<br />";
-for($i=0, $size=count($data_tab); $i<$size; $i++){
-echo "Trame $i: $data_tab[$i]<br />";
+
+function viewOriginalLog ($data) {
+    echo "Raw Data:<br />";
+    echo("$data");
+}
+
+function viewTabLogs ($data){
+    $data_tab = str_split($data,33);
+    echo "Tabular Data:<br />";
+    for($i=0, $size=count($data_tab); $i<$size; $i++){
+        echo "Trame $i: $data_tab[$i]<br />";
+    }
 }
 
 $trame = $data_tab[1];
