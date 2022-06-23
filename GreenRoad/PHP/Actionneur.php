9665f3a24a6a18de5597dd9f024078d07f9e3820
AchrafJALAL <37595402+ABUNAI27@users.noreply.github.com>
2022-06-23 16:19:16 +0200

    Update Actionneur.php

--- GreenRoad/PHP/Actionneur.php
+++ GreenRoad/PHP/Actionneur.php
@@ -48,6 +48,17 @@
         </div>
 
     </nav>
+ <?php
+    $ch = curl_init();
+    curl_setopt(
+    $ch,
+    CURLOPT_URL,
+    "http://projets-tomcat.isep.fr:8080/appService/?ACTION=GETLOG&TEAM=G3-B");
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $data = curl_exec($ch);
+    curl_close($ch);
+?>
 <div class="container">
   <div class="Données-brutes">
     
